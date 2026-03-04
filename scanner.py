32b79bfea6e1de1d76cce7ab6fe5e18bf313e6ac
Ahlyx <alexdoll929@gmail.com>
2026-03-03 18:26:58 -0700

    phase 1 complete - ARP scan working

--- scanner.py
+++ scanner.py
@@ -1,17 +1,12 @@
 from scapy.all import ARP, Ether, srp
 
 
-
 def scan(subnet):
     arp = ARP(pdst=subnet)
     ether = Ether(dst="ff:ff:ff:ff:ff:ff")
     packet = ether / arp
 
-    print(f"Sending packets on interface: {r'\Device\NPF_{54127133-D621-44A7-ACF7-2170E801C8AA}'}")
-    print(f"Packet summary: {packet.summary()}")
-
-    # verbose=True this time so we can see what's happening
-    answered, unanswered = srp(packet, iface=r"\Device\NPF_{54127133-D621-44A7-ACF7-2170E801C8AA}", timeout=2, verbose=True)
+    answered, unanswered = srp(packet, iface="ens33", timeout=2, verbose=True)
     
     print(f"\nAnswered: {len(answered)}")
     print(f"Unanswered: {len(unanswered)}")
